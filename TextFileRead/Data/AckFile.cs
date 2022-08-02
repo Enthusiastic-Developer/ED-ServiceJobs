f2baee734bd39670f94a4af96f2a67e111da073c
Nikhil Banuri <nbanuri@tollplus.com>
2022-08-02 17:29:46 +0530

    Project is created and added

---
+++ TextFileRead/Data/AckFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileRead.Data
+{
+    public class AckFile
+    {
+        public string? FileName { get; set; }
+        public DateTime? ProcessedDate { get; set; }
+        public List<DataInFile>? DataInFiles { get; set; }
+    }
+
+    public class DataInFile
+    {
+        public string? TextFileName { get; set; }
+        public string? ProcessedDateTime { get; set; }
+        public string? FileName { get; set; }
+        public string? RecordCode { get; set; }
+        public string? FileSize { get; set; }
+        public string? StatusCode { get; set; }
+        public string? ProcessingStatus { get; set; }
+    }
+}
---
+++ TextFileRead/Domain/ProcessFile.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextFileRead.Services;
+
+namespace TextFileRead.Domain
+{
+    public class ProcessFile
+    {
+        private readonly ILogger _logger = new NLogLoggerFactory().CreateLogger<ProcessFile>();
+        public void TextFileProcess()
+        {
+            _logger.LogInformation("Processing file started");
+            Helper helper = new Helper();
+            try
+            {
+                helper.GetReturnAckFiles();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error processing file");
+            }
+            _logger.LogInformation("Processing file ended");
+        }
+    }
+}
